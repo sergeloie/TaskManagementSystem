3d5ea5e848f7580a6bb4454b17ebe952d6fca210
daBoss <serge.loie@yandex.ru>
2024-08-07 22:47:43 +0500

    update User entity

--- src/main/java/ru/anseranser/TaskManagementSystem/model/User.java
+++ src/main/java/ru/anseranser/TaskManagementSystem/model/User.java
@@ -49,11 +49,11 @@ public class User implements UserDetails {
 
     @Override
     public String getPassword() {
-        return "";
+        return this.password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return this.username;
     }
 }
