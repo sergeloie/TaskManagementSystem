a52595d8c942e6c7111a07091e6bb77f66283807
daBoss <serge.loie@yandex.ru>
2024-09-14 00:01:34 +0500

    @Transaction

--- src/main/java/ru/anseranser/task_management_system/TaskManagementSystemApplication.java
+++ src/main/java/ru/anseranser/task_management_system/TaskManagementSystemApplication.java
@@ -3,8 +3,10 @@ package ru.anseranser.task_management_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableJpaAuditing
+@EnableTransactionManagement
 @SpringBootApplication
 public class TaskManagementSystemApplication {
 
