5ff33d4de533773a0361b1fa05c3db3b2a97d602
daBoss <serge.loie@yandex.ru>
2024-09-13 23:39:08 +0500

    Optimize imports

--- src/main/java/ru/anseranser/task_management_system/controller/TaskCommentaryResource.java
+++ src/main/java/ru/anseranser/task_management_system/controller/TaskCommentaryResource.java
@@ -1,6 +1,5 @@
 package ru.anseranser.task_management_system.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
@@ -12,10 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 import ru.anseranser.task_management_system.dto.commentary.TaskCommentaryCreateDto;
 import ru.anseranser.task_management_system.dto.commentary.TaskCommentaryDto;
@@ -34,8 +33,6 @@ public class TaskCommentaryResource {
 
     private final TaskCommentaryRepository taskCommentaryRepository;
     private final UserUtils userUtils;
-
-    private final ObjectMapper objectMapper;
     private final TaskCommentaryMapper taskCommentaryMapper;
 
     @GetMapping
--- src/main/java/ru/anseranser/task_management_system/controller/UserResource.java
+++ src/main/java/ru/anseranser/task_management_system/controller/UserResource.java
@@ -1,6 +1,5 @@
 package ru.anseranser.task_management_system.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
@@ -14,35 +13,24 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.server.ResponseStatusException;
 import ru.anseranser.task_management_system.dto.user.UserCreateDto;
 import ru.anseranser.task_management_system.dto.user.UserDto;
 import ru.anseranser.task_management_system.dto.user.UserUpdateDto;
-import ru.anseranser.task_management_system.mapper.UserMapper;
-import ru.anseranser.task_management_system.model.User;
-import ru.anseranser.task_management_system.repository.UserRepository;
 import ru.anseranser.task_management_system.service.UserService;
-import ru.anseranser.task_management_system.util.UserUtils;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserResource {
 
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
-    private final UserUtils userUtils;
     private final UserService userService;
-
-    private final ObjectMapper objectMapper;
 
     @GetMapping
     public Page<UserDto> getList(@ParameterObject Pageable pageable) {
